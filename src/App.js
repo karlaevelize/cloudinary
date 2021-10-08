3dfbe7de59cc5de603dabe1a730d44dae29d0eda
karlaevelize <k.evelize@gmail.com>
2021-10-08 15:24:04 +0200

    coudinary example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  const [image, setImage] = useState("")
+  
+  const uploadImage = async(e) => {
+    console.log("triggered")
+    const files = e.target.files
+    const data = new FormData()
+    data.append("file", files[0])
+    data.append('upload_preset', "lpsty2kc")
+    //first parameter is always upload_preset, second is the
+    setLoading(true)
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/delvoxvyc/image/upload", {
+      method: "POST",
+      body: data
+    })
+
+    const file = await res.json()
+    console.log("file", file)
+    setImage(file.url)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cloudinary Upload</h1>
+      <input type="file" name="file" placeholder="drag it here" onChange={uploadImage}/>
+      <img src={image}/>
     </div>
   );
 }
